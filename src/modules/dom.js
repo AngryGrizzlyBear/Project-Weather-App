8d2219435d7570b78f489ee3377bbf0b9cc20d90
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2025-09-17 12:25:31 -0700

    Added Front Page Functionality

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,19 +1,32 @@
 import { fetchWeather } from "./api";
 
+function renderWeather(data) {
+    const output = document.getElementById("weather-output");
+    output.innerHTML = `
+    <h2>Weather for ${data.location}</h2>
+    <p><strong>Date:</strong>${data.date}</p>
+    <p><strong>Temperature:</strong>${data.temperature} °C</p>
+    <p><strong>High:</strong>${data.tempMax} °C <strong>Low:</strong>${data.tempMin} °C</p>
+    <p><strong>Conditions:</strong>${data.conditions}</p>
+    <p><strong>Description:</strong>${data.description}</p>
+    `
+}
+
 export function setupForm() {
     const form = document.getElementById("location-form");
     const input = document.getElementById("location-input");
 
-    form.addEventListener("Submit", async (e) => {
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        const location = input.ariaValueMax.trim();
+        const location = input.value.trim();
         if (!location) return;
 
-        console.log(`Fetching weather for: $(location)`);
+        console.log(`Fetching weather for: ${location}`);
         const data = await fetchWeather(location);
 
         if (data) {
             console.log("Processed Data from form submit:", data);
+            renderWeather(data);
         }
     });
 }
